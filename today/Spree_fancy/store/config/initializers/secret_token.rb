d5fdf13a88fbe46bc442bf8b5ba0c442900cc2ee
piyushanchanto <piyush.awasthi@anchanto.com>
2014-01-06 17:40:02 +0530

    spree fancy is compleated

---
+++ today/Spree_fancy/store/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = '0581f1266828b6504e48bb5519372ccefbe550a5fe2226eae1f137ef1e5cfb630fdfd9243d62146b376609ec1c4453c22010be582917fccd145c0efa981ed2a9'
